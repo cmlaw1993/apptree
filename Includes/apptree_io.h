b90e06f1e07b22de5cecf40df2ed3f00dfeb6f48
Dennis Law <cmlaw1993@gmail.com>
2016-06-07 18:29:45 +0800

    Added apptree_io.c and apptree_io.h
    These two files will be used to implement the io functionlity for
    the apptree.

---
+++ Includes/apptree_io.h
@@ -0,0 +1,13 @@
+
+/** @file apptree_io.h
+ *  @brief Header file for apptree_io.c
+ *  @author Dennis Law
+ *  @date April 2016
+ */
+
+#ifndef APPTREE_IO_H
+#define APPTREE_IO_H
+
+#include "apptree.h"
+
+#endif	/* APPTREE_IO_H */
---
+++ Sources/apptree_io.c
@@ -0,0 +1,8 @@
+
+/** @file apptree_io.c
+ *  @brief Implements the io functionlities for the apptree.
+ *  @author Dennis Law
+ *  @date June 2016
+ */
+
+#include "apptree_io.h"
